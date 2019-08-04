8ac23cdc07d96855d20296e449fffd23572fde25
MasatoShima <masato.shima@icloud.com>
2019-08-04 22:28:03 +0900

    新規作成

---
+++ 20190522_Lambda_StepFunctions_Sample/deploy.sh
@@ -0,0 +1,22 @@
+# Name: Masato Shima
+# Created by: 2019/08/04
+
+# Upload statemachine.yaml
+aws s3 cp statemachine.yaml s3://mybucket-deploy/statemachine.yaml
+
+# package
+sam package \
+ --template-file template.yaml \
+ --s3-bucket mybucket-deploy \
+ --output-template-file template-packaged.yaml \
+ --profile default
+
+# deploy
+sam deploy \
+ --template-file template-packaged.yaml \
+ --stack-name Samples-AWS-Serverless-Application \
+ --profile default \
+ --region ap-northeast-1 \
+ --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM
+
+# End
