d11066fa5ea105ec424164dc452ba12f32c76191
MasatoShima <masato.shima@icloud.com>
2019-09-01 18:04:36 +0900

    修正

--- 20190522_Functions_chaining/deploy.sh
+++ 20190522_Functions_chaining/deploy.sh
@@ -2,20 +2,20 @@
 # Created on: 2019/08/04
 
 # Upload statemachine.yaml
-aws s3 cp statemachine.yaml s3://mybucket-deploy/statemachine.yaml
+aws s3 cp statemachine.yaml s3://mybucket-deploy/statemachine.yaml --profile private
 
 # package
 sam package \
  --template-file template.yaml \
  --s3-bucket mybucket-deploy \
  --output-template-file template-packaged.yaml \
- --profile default
+ --profile private
 
 # deploy
 sam deploy \
  --template-file template-packaged.yaml \
  --stack-name Samples-AWS-Serverless-Application \
- --profile default \
+ --profile private \
  --region ap-northeast-1 \
  --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM
 
